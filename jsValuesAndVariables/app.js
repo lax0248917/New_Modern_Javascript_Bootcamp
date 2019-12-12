dd709198028acc9e2bf6e5abfc03e6763f9346b3
lax0248917 <lax0248917@gmail.com>
2019-12-12 12:03:08 -0500

    Section 2 final commit

---
+++ jsValuesAndVariables/app.js
@@ -0,0 +1,52 @@
+// Variables & Let
+
+let age = 72;
+age // returns 72
+let hens = 4;
+let roosters = 2;
+console.log(hens + roosters) // returns 6
+
+// Unary Operators
+let score = 0;
+score = score + 10;
+score //returns 10
+
+score += 1;
+score // returns 11 
+
+score -= 100;
+score // returns -89
+
+score = 150;
+let bonusMult = 3;
+score *= bonusMult;
+score // returns 450
+
+let counter = 0;
+counter++;
+counter // returns 1
+
+let sum = 4;
+sum--;
+sum // returns 3
+
+// Introducing Const
+
+const year = 1973;
+year +=1 // returns error
+
+//when to use const over let
+const pi = 3.14159
+const daysInWeek = 7;
+const minHeightForRide = 60;
+// in situations where you don't want to change the variable.
+// can not have variables with the same name regardless of const or let
+// var is available but not widely used today
+var eggs = 12;
+eggs // returns 12
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+# New Modern Javascript Bootcamp 2020
+
+## This readme is for an overview of the entire course.
+
+### All notes will be added to better document my experience as I work through this course. This notes will be made public in Github and the repo that has been allocated to track my progress.
+
+- [x] Initiate the Git Repo for this course
+- [x] Maintain source control through the entirety of the course
