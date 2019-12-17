dbfb6e557dccd1071a2dcd5a382060ec18a45142
lax0248917 <lax0248917@gmail.com>
2019-12-17 15:53:40 -0500

    Section 5 second commit

--- captureCollectionsOfDataWithArrays/app.js
+++ captureCollectionsOfDataWithArrays/app.js
@@ -1,3 +1,25 @@
 // Capture Collections of Data with Arrays
 
 //Creating Arrays
+// To make an empty array
+let students = [];
+// An array of strings
+let colors = ['red', 'orange', 'yellow'];
+// An Array of mumbers 
+let lottoNums = [19,22,56,12,51];
+// A mixed Array 
+let stuff = [true, 68, 'cat', null];
+
+// Example 1 
+let shoppingList = ['cereal', 'cheese', 'ice'];
+
+// Array Indices
+// arrays are indexed just like strings are, with the first value with an index of 0
+shoppingList[1]; //returns "cheese"
+shoppingList[0]; //returns "cereal"
+shoppingList[8]; //returns "undefined"
+shoppingList.length; //returns 3
+
+//Modifying Arrays
+
+
